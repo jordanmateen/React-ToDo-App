c5060f4e2a24b8a6e9df976c8313d3f59e22ced6
Jordan Mateen <jodanmateen1991@gmail.com>
2019-02-16 10:39:39 -0500

    delet button fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import Todos from './Todos'
 
 
 class App extends Component {
+  state  = {
+    todo : [
+      {id:1 , content: 'Study React'},
+      {id:2 , content: 'build cool stuff'}
+    ]
+  }
+
+  deleteTodo = (id) =>{
+    const todos = this.state.todo.filter( todo =>{
+      return todo.id !== id
+    });
+
+    this.setState({
+      todo : todos 
+    })
+  }
   render() {
     return (
-      <div className="App">
-       
+      <div className="App container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos = {this.state.todo} deleteTodo = {this.deleteTodo}/>
       </div>
     );
   }
